1faa386169e2342533cd078f4a7cd38dd4dfafec
Kodok <codyguy.id@gmail.com>
2024-04-19 16:25:38 +0700

    add bootstrap, add header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <h1>Welcome to My Shop!</h1>;
-}
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to My Shop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
