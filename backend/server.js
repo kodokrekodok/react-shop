1d3190ce9d54c3bb2700ecb50829741de1325edc
Kodok <codyguy.id@gmail.com>
2024-04-20 19:40:47 +0700

    add model, error middleware and controller

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import express from "express";
 dotenv.config();
 
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -13,14 +14,9 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-// console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
