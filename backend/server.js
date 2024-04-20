c47fb565753ceb0dd31ce247cb45d8e428a1608e
Kodok <codyguy.id@gmail.com>
2024-04-20 11:22:37 +0700

    add mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import dotenv from "dotenv";
 import express from "express";
-import products from "./data/products.js";
 dotenv.config();
+
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
