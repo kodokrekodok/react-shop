fc745446c4355c150c08f091013fcb100b5acc56
Kodok <codyguy.id@gmail.com>
2024-04-22 11:52:43 +0700

    add backend authentication

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 dotenv.config();
@@ -5,10 +6,18 @@ dotenv.config();
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 const port = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -18,5 +27,7 @@ app.use("/api/products", productRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
+
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
